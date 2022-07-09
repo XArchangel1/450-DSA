56191eab1b76611e7bcb7b2834df8f7f8c641014
Nakshatra Semwal <nakshatrasemwal1@gmail.com>
2022-07-09 12:05:57 +0530

    hard delete kth node from last

---
+++ Linked List/deletekthfromlasthard.cpp
@@ -0,0 +1,36 @@
+LinkedListNode<int> *removeKthNode(LinkedListNode<int> *head, int K)
+{
+    if (head == NULL || K == 0)
+    {
+        return head;
+    }
+
+    LinkedListNode<int> *slow = head;
+    LinkedListNode<int> *fast = head;
+
+    for (int i = 0; i < K; i++)
+    {
+        if (fast->next == NULL) 
+        {
+            LinkedListNode<int> *next = head->next;
+            head->next = NULL;
+            head = next;
+            return head;
+        }
+        
+        else
+        {
+            fast = fast->next;
+        }
+    }
+
+    while (fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    slow->next = slow->next->next;
+
+    return head;
+}
