77174d89c40c0cd27b417842b7fe7a27c914a56d
XArchangel1 <nakshatrasemwal1@gmail.com>
2022-07-18 13:42:53 +0530

    connect nodes

---
+++ Trees/connectNodes.cpp
@@ -0,0 +1,33 @@
+#include<queue>
+void connectNodes(BinaryTreeNode< int > *root) 
+{
+    if(root == nullptr)
+    {
+        return;
+    }
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int s = q.size();
+        for(int i = 1;i<=s;i++)
+        {
+            BinaryTreeNode<int>* temp = q.front();
+            q.pop();
+            if(i<s)
+            {
+                temp->next = q.front();
+            }
+            
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
