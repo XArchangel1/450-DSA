ced648497a053830aa0a9c75be34cc29da9268c5
XArchangel1 <nakshatrasemwal1@gmail.com>
2022-07-19 13:36:56 +0530

    kth smallest in BST

---
+++ Trees/kthSmallest.cpp
@@ -0,0 +1,32 @@
+void help(TreeNode<int> *root,vector<int> &ans)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    
+    help(root->left,ans);
+    ans.push_back(root->data);
+    help(root->right,ans);
+}
+
+int kthSmallest(TreeNode<int>* root, int k) 
+{
+    if(root == NULL || k == 0)
+    {
+        return -1;
+    }
+    
+    vector<int> arr;
+    help(root,arr);
+    int ans = k-1;
+    if(ans>=0)
+    {
+       return arr[ans];
+    }
+    
+    else
+    {
+        return -1;
+    }
+}
